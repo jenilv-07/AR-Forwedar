b528f28b7e5d1ea502c3142fe25e70c7b7c59211
jenilv-07 <vekariyajenil888@gmail.com>
2024-07-31 14:14:22 +0530

    remove the unwanted code

--- ar_frowedar/socket_anget_conf/send_config.py
+++
@@ -1,8 +0,0 @@
-
-
-agent_id = "001"
-
-dest_socket = "/var/ossec/queue/sockets/wmodules"
-
-        # Simple socket message
-msg = f"{str(agent_id).zfill(3)} {component} {GETCONFIG_COMMAND} {configuration}"
